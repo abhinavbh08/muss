233a9d4397783bb43c8092cd04dbdf5c3c3f3b8e
abhinavbh08 <abhinavbhatt.999@gmail.com>
2022-10-03 12:06:15 +0200

    chnage save intervals

--- muss/fairseq/base.py
+++ muss/fairseq/base.py
@@ -108,8 +108,8 @@ def fairseq_train(
         --criterion {criterion} --label-smoothing 0.1
         --lr-scheduler {lr_scheduler} --lr {lr} --warmup-updates {warmup_updates} --update-freq {update_freq}
         --arch {arch} --dropout {dropout} --weight-decay 0.0 --clip-norm 0.1 --share-all-embeddings
-        --no-epoch-checkpoints --save-interval 999999 --validate-interval 999999
-        --max-update {max_update} --save-interval-updates {save_interval_updates} --keep-interval-updates 1 --patience 10
+        --no-epoch-checkpoints --save-interval 9999 --validate-interval 999999
+        --max-epoch 4 --no-last-checkpoints --patience 10
         --batch-size {max_sentences} --seed {seed}
         --distributed-world-size {ngpus} --distributed-port {distributed_port}
         '''
