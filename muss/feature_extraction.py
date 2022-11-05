d407fcf1fec14d3f1823d26784f90b63bfc126d1
abhinavbh08 <abhinavbhatt.999@gmail.com>
2022-11-05 09:53:02 +0100

    featu ext

--- muss/feature_extraction.py
+++ muss/feature_extraction.py
@@ -24,7 +24,6 @@ def get_word2rank(vocab_size=10 ** 5, language='en'):
             break
         word = line.split(' ')[0]
         word2rank[word] = i
-    print("Word2Rank size", len(word2rank))
     return word2rank
 
 def get_rank(word, language='en'):
